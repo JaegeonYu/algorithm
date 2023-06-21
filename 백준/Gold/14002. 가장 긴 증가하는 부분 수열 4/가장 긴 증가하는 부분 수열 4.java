4ce26ad752a8d08fabeac7687d43327666357d41
Jack <76714219+JaegeonYu@users.noreply.github.com>
2023-06-21 13:45:02 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 144 ms, Memory: 14600 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main{
+	static int n, a[], lis[];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(bf.readLine());
+		a = new int[n];
+		lis = new int[n];
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		List<Integer> bs = new ArrayList<>();
+		for(int i=0;i<n;i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+			if(bs.isEmpty() || bs.get(bs.size()-1) < a[i]) {
+				lis[i]=bs.size();
+				bs.add(a[i]);
+			}else {
+				int idx = Collections.binarySearch(bs, a[i]);
+				idx = idx >=0? idx : -idx-1;
+				lis[i]=idx;
+				bs.set(idx, a[i]);
+			}
+		}
+		int idx = bs.size()-1;
+		Stack<Integer> stack = new Stack<>();
+		for(int i=n-1;i>=0;i--) {
+			if(lis[i]==idx) {
+				idx--;
+				stack.push(a[i]);
+			}
+		}
+		System.out.println(bs.size());
+		while(!stack.isEmpty()) {
+			System.out.print(stack.pop() +" ");
+		}
+	}
+
+}
