b34b76ef550120bcec63a1cc3c21fe7e73297794
Jack <76714219+JaegeonYu@users.noreply.github.com>
2023-12-07 09:29:54 +0900

    [Silver I] Title: 쿼드트리, Time: 132 ms, Memory: 14652 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,48 +1,45 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-	static int N;
-	static char a[][];
+	static int N, a[][];
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		N = Integer.parseInt(br.readLine());
-		a = new char[N][N];
+		a = new int[N][N];
+		
 		for(int i=0;i<N;i++) {
 			String tmp = br.readLine();
 			for(int j=0;j<N;j++) {
-				a[i][j] = tmp.charAt(j);
+				a[i][j] = tmp.charAt(j)-'0';
 			}
 		}
 		
-		String ret = dfs(0, 0, N);
-		System.out.println(ret);
+		System.out.println(divide(N, 0, 0));
 	}
-	private static String dfs(int y, int x, int size) {
-		if(size == 1) {
+	private static String divide(int n, int y, int x) {
+		if(n==1) {
 			return String.valueOf(a[y][x]);
-		}
-		StringBuilder tmp = new StringBuilder();
-		for(int i=y;i<y+size;i++) {
-			for(int j=x;j<x+size;j++) {
-				if(a[i][j] != a[y][x]) {
-					tmp.append("(");
-					tmp.append(dfs(y,x, size/2));
-					tmp.append(dfs(y,x+size/2, size/2));
-					tmp.append(dfs(y+size/2, x, size/2));
-					tmp.append(dfs(y+size/2, x+size/2, size/2));
-					tmp.append(")");
-					return tmp.toString();
+		}else {
+			int now = a[y][x];
+			for(int i=0;i<n;i++) {
+				for(int j=0;j<n;j++) {
+					if(a[y+i][x+j]!=now) {
+						// 4등분하고 리턴
+						StringBuilder sb = new StringBuilder("(");
+						sb.append(divide(n/2, y, x));
+						sb.append(divide(n/2, y, x+n/2));
+						sb.append(divide(n/2, y+n/2, x));
+						sb.append(divide(n/2, y+n/2, x+n/2));
+						sb.append(")");
+						return sb.toString();
+					}
 				}
 			}
+			return String.valueOf(a[y][x]);
 		}
-		
-		return String.valueOf(a[y][x]);
 	}
 
 }
