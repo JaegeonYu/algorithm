98258f23defac5cfc00c1a4374ac0dfe45313f5e
Jack <76714219+JaegeonYu@users.noreply.github.com>
2023-12-22 15:07:59 +0900

    [Silver I] Title: 회의실 배정, Time: 504 ms, Memory: 43132 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,20 +1,16 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, a[][];
-	public static void main(String[] args) throws IOException {
+	static int N;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		N = Integer.parseInt(br.readLine());
-		a = new int[N][2];
+		int a[][] = new int[N][2];
 		
 		for(int i=0;i<N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,23 +18,21 @@ public class Main {
 			a[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(a, new Comparator<int []>() {
-
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if(o1[1] == o2[1])return o1[0]-o2[0];
-				return o1[1] - o2[1];
+		Arrays.sort(a, (o1, o2)->{
+			if(o1[1]==o2[1]) {
+				return Integer.compare(o1[0], o2[0]);
 			}
+			return Integer.compare(o1[1], o2[1]);
 		});
 		
-		int ret = 1;
-		int last = a[0][1];
+		int now = 0;
+		int ret = 0;
 
-		for(int i=1;i<N;i++) {
-			if(a[i][0] < last)continue;
-			else {
-				last = a[i][1];
+		
+		for(int i=0;i<N;i++) {
+			if(now <= a[i][0]) {
 				ret++;
+				now = a[i][1];
 			}
 		}
 		
