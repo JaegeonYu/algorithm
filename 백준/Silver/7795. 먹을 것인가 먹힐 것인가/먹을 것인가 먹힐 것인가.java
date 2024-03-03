9736dc034a8f18268bda60ab7dfbbd7594e3e914
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-03-03 13:46:31 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 428 ms, Memory: 34360 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,47 +5,36 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int t, n, m, a[], b[];
+	static int T, n, m, a[], b[];
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		t = Integer.parseInt(bf.readLine());
-		for(int T=0;T<t;T++) {
-			StringTokenizer st = new StringTokenizer(bf.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		for(int t=0;t<T;t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(st.nextToken());
 			m = Integer.parseInt(st.nextToken());
+			
 			a = new int[n];
 			b = new int[m];
-			st = new StringTokenizer(bf.readLine());
-			for(int i=0;i<n;i++)a[i]=Integer.parseInt(st.nextToken());
-			st = new StringTokenizer(bf.readLine());
-			for(int i=0;i<m;i++)b[i]=Integer.parseInt(st.nextToken());
-			
+			st = new StringTokenizer(br.readLine());
+			for(int i=0;i<n;i++)a[i] = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			for(int i=0;i<m;i++)b[i] = Integer.parseInt(st.nextToken());
 			
 			Arrays.sort(a);
 			Arrays.sort(b);
-			int ret = 0;
 			
-			int j =0 ;
+			int tmp = 0 ;
+			int ret = 0;
 			for(int i=0;i<n;i++) {
-				int le = 0;
-				int ri = m-1;
-				int idx = 0;
-				while(le <= ri) {
-					int mid = (le+ri)/2;
-					if(b[mid]<a[i]) {
-						le =mid+1;
-						idx = mid+1;
-					}else ri = mid -1;
+				while(tmp < m && a[i] > b[tmp]) {
+					tmp++;
 				}
-				ret += idx;
-				
+				ret += tmp;
 			}
 			System.out.println(ret);
-			
-			
 		}
 		
-
 	}
 
 }
