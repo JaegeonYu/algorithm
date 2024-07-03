4d362c8ed94e822c6c767735ee4a0d22303256b2
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-07-03 15:46:25 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14588 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,41 +5,30 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, a[][], ret = Integer.MAX_VALUE;
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static int N, arr[][], dp[][];
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		a = new int[N][3];
-		
+		arr = new int[N][3];
+		dp = new int[N][3];
 		for(int i=0;i<N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			a[i][0] = Integer.parseInt(st.nextToken());
-			a[i][1] = Integer.parseInt(st.nextToken());
-			a[i][2] = Integer.parseInt(st.nextToken());
-		}
-		int dp[][] = new int[N][3];
-		dp[0][0] = a[0][0];
-		dp[0][1] = a[0][1];
-		dp[0][2] = a[0][2];
-		
-		for(int i=1;i<N;i++) {
 			for(int j=0;j<3;j++) {
-				int min = Integer.MAX_VALUE;
-				for(int k=0;k<3;k++) {
-					if(j == k)continue;
-					if(min > dp[i-1][k]) {
-						min = dp[i-1][k];
-					}
-				}
-				dp[i][j] = min + a[i][j];
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		for(int i=0;i<3;i++) {
-			ret = Math.min(ret, dp[N-1][i]);
+		dp[0] = arr[0];
+	
+		for(int i=1;i<N;i++) {
+			for(int j=0;j<3;j++) {
+				int prev = Math.min(dp[i-1][(j+1) % 3], dp[i-1][(j+2) % 3]);
+				dp[i][j] = prev + arr[i][j];
+			}
 		}
 		
-		System.out.println(ret);
+		System.out.println(Math.min(Math.min(dp[N-1][0], dp[N-1][1]),dp[N-1][2]));
+		
 	}
 
 }
