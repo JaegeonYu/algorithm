573d61dde191255ad563bebee96ca1540398485a
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-07-05 14:39:59 +0900

    [Silver I] Title: 보석 상자, Time: 336 ms, Memory: 34580 KB -BaekjoonHub

--- 백준/Silver/2792. 보석 상자/보석 상자.java
+++ 백준/Silver/2792. 보석 상자/보석 상자.java
@@ -4,45 +4,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, a[], max, ret = Integer.MAX_VALUE;
+	static int N, M, arr[], ret = Integer.MAX_VALUE;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
-		a = new int[M];
+		arr = new int[M];
 		for(int i=0;i<M;i++) {
-			a[i] = Integer.parseInt(br.readLine());
-			max = Math.max(a[i], max);
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
 		int le = 1;
-		int ri = max;
+		int ri = Integer.MAX_VALUE-1;
 		while(le <= ri) {
 			int mid = (le+ri)/2;
+			
 			if(check(mid)) {
-				ret = Math.min(ret, mid);
+				ret = mid;
 				ri = mid -1;
-			}else {
-				le = mid + 1;
-			}
+				
+			}else le = mid +1;
 		}
 		
 		System.out.println(ret);
-		
 	}
 	private static boolean check(int mid) {
+
 		int cnt = 0;
 		for(int i=0;i<M;i++) {
-			int now = a[i];
-			int div = now / mid;
-			int mod = now % mid;
-			cnt += div;
-			if(mod > 0) {
-				cnt++;
-			}
+			cnt += arr[i] / mid;
+			if(arr[i] % mid > 0)cnt++;
 		}
+
 		return N >= cnt;
 	}
 
