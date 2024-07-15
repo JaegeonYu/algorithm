f600493660e04c34f9d9c15013dd0d232982f874
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-07-15 13:37:43 +0900

    [Gold I] Title: 외판원 순회, Time: 192 ms, Memory: 20060 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -1,49 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static final int MAX_N = 16;
-    private static final int INF = 987654321;
-    private static int n;
-    private static int[][] dp = new int[MAX_N][1 << MAX_N];
-    private static int[][] dist = new int[MAX_N][MAX_N];
+	static int N, w[][], dp[][], ret = Integer.MAX_VALUE;
+	static boolean vis[];
+	 private static final int INF = 987654321;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		vis = new boolean[N];
+		w = new int[N][N];
+		dp = new int[N][1 << N];
+		
+		for(int i=0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++) {
+				w[i][j] = Integer.parseInt(st.nextToken());
+			
+			}
+		}
+		
+		for(int i=0;i<N;i++)Arrays.fill(dp[i], -1);
+	
+		System.out.println(tsp(0, 1));
+	}
+	private static int tsp(int here, int visited) {
+		if(visited == (1 << N) -1) {
+			return w[here][0] != 0 ? w[here][0] : INF;
+		}
+		
+		if(dp[here][visited] != -1)return dp[here][visited];
+		
+		dp[here][visited] =  INF;
+		for(int i=0;i<N;i++) {
+			if((visited & (1<< i)) != 0)continue;
+			if(w[here][i] == 0)continue;
+			dp[here][visited] = Math.min(dp[here][visited], tsp(i, visited | (1<<i)) + w[here][i]);
+		}
+		return dp[here][visited];
+	}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        n = scanner.nextInt();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dist[i][j] = scanner.nextInt();
-            }
-        }
-
-        for (int i = 0; i < MAX_N; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-
-        System.out.println(tsp(0, 1));
-    }
-
-    private static int tsp(int here, int visited) {
-        if (visited == (1 << n) - 1) {
-            return dist[here][0] != 0 ? dist[here][0] : INF;
-        }
-
-        if (dp[here][visited] != -1) {
-            return dp[here][visited];
-        }
-
-        dp[here][visited] = INF;
-        for (int i = 0; i < n; i++) {
-            if ((visited & (1 << i)) != 0) {
-                continue;
-            }
-            if (dist[here][i] == 0) {
-                continue;
-            }
-            dp[here][visited] = Math.min(dp[here][visited], tsp(i, visited | (1 << i)) + dist[here][i]);
-        }
-        return dp[here][visited];
-    }
 }
