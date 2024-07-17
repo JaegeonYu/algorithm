6e02868423b6565d5b99d853f1a7f6ed2391f153
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-07-17 09:39:55 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -4,44 +4,47 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, arr[][], dp[][][];
-	static int dy[] = {0,1,1};
-	static int dx[] = {1,0,1};
-	public static void main(String[] args) throws IOException {
+	static int N, a[][], dp[][][];
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N][N];
+		a = new int[N][N];
 		dp = new int[N][N][3];
-		
 		for(int i=0;i<N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j=0;j<N;j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				a[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+		// 0 -> 가로, 1-> 세로, 2-> 대각
 		dp[0][1][0] = 1;
 		for(int i=0;i<N;i++) {
 			for(int j=2;j<N;j++) {
-				if(arr[i][j] == 1)continue;
-				// 0 -> x 축, 1 -> y축 , 2-> 대각선
-				dp[i][j][0] = dp[i][j-1][0]+dp[i][j-1][2];
+				if(a[i][j] == 1)continue;
 				
-				if(i == 0)continue;
-				dp[i][j][1] = dp[i-1][j][1]+dp[i-1][j][2];
-				
-				if(arr[i-1][j] ==1 || arr[i][j-1] == 1)continue;
-				dp[i][j][2] = dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2];
+				if(i == 0) {
+					dp[i][j][0]=dp[i][j-1][0];
+				}else {
+					dp[i][j][0] = dp[i][j-1][2]+dp[i][j-1][0];
+					dp[i][j][1] = dp[i-1][j][2]+dp[i-1][j][1];
+					if(a[i-1][j] != 1 && a[i][j-1] != 1 && a[i-1][j-1] != 1)dp[i][j][2] = dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2];
+				}
 			}
 		}
+//		for(int i=0;i<N;i++) {
+//			for(int j=0;j<N;j++) {
+//				int sum = 0;
+//				for(int k=0;k<3;k++) {
+//					sum += dp[i][j][k];
+//				}
+//				System.out.print(sum+" ");
+//			}
+//			System.out.println();
+//		}
 		
-		int ret = 0 ;
-		for(int i=0;i<3;i++) {
-			ret += dp[N-1][N-1][i];
-		}
+		System.out.println(dp[N-1][N-1][0]+dp[N-1][N-1][1]+dp[N-1][N-1][2] );
+		
 
-		System.out.println(ret);
 	}
-
 
 }
