9780c7323b2ace713d412d7b2bf84ece295de6b1
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-07-23 10:56:35 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 15748 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,37 +1,30 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main{
-	static int N, K;
-	static int[][] products, dp;
-	
+public class Main {
+
+	static int N, K, dp[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		
-		products = new int[N][2];
-		
-		for(int i=0;i<N;i++) {
+		dp = new int[100004];
+	
+		for(int i=1;i<=N;i++) {
 			st = new StringTokenizer(br.readLine());
-			products[i][0] = Integer.parseInt(st.nextToken());
-			products[i][1] = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			for(int j=K;j>=w;j--) {
+				dp[j] = Math.max(dp[j], dp[j-w]+v);
+			}	
 		}
 		
-		dp = new int[N+1][K+1];
-		for(int i=1;i<N+1;i++) {
-			for(int j=1;j<K+1;j++) {
-				if(products[i-1][0] > j) {
-					dp[i][j] = dp[i-1][j];
-				}else {
-					dp[i][j] = Math.max(products[i-1][1] + dp[i-1][j-products[i-1][0]], dp[i-1][j]);
-				}
-			}
-		}
-		System.out.println(dp[N][K]);
+		System.out.println(dp[K]);
 	}
+
 }
