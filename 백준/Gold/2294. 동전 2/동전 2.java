953dfc48a41b5cc6e6554c99e5027d7441440e50
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-07-24 09:13:39 +0900

    [Gold V] Title: 동전 2, Time: 124 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -2,31 +2,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, K, dp[];
-	static Set<Integer> set = new HashSet<>();
-	public static void main(String[] args) throws IOException {BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N,K ,dp[];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		dp = new int[K+1];
+		dp = new int[100004];
 		
-		Arrays.fill(dp, 987654321);
-		dp[0] = 0;
+		Arrays.fill(dp, Integer.MAX_VALUE-5);
 		for(int i=0;i<N;i++) {
 			int now = Integer.parseInt(br.readLine());
+			dp[now] = Math.min(dp[now], 1);
 			for(int j=now;j<=K;j++) {
 				dp[j] = Math.min(dp[j], dp[j-now]+1);
 			}
 		}
 		
-		if(dp[K] == 987654321)System.out.println(-1);
+		if(dp[K] == Integer.MAX_VALUE-5)System.out.println(-1);
 		else System.out.println(dp[K]);
-
+				
 	}
 
 }
