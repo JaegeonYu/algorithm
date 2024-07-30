ff50c6a8436f1643a7bef6cc08fc75ed030b3217
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-07-30 09:50:10 +0900

    [Gold II] Title: 색종이 붙이기, Time: 236 ms, Memory: 23964 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -1,79 +1,71 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int a[][], ret = Integer.MAX_VALUE;
-	static boolean vis[][], flag = true;
-	static int paper[] = {0, 5, 5, 5, 5, 5};
+	static int[] paper = {0,5,5,5,5,5};
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		a = new int[10][10];
-		
-		for(int i=0;i<10;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=0;j<10;j++) {
-				a[i][j] = Integer.parseInt(st.nextToken());
-			}
+	a = new int[10][10];
+	for(int i=0;i<10;i++) {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int j=0;j<10;j++) {
+			a[i][j] = Integer.parseInt(st.nextToken());
 		}
-		
-		dfs(0,0,0);
-		if(ret == Integer.MAX_VALUE)System.out.println(-1);
-		else System.out.println(ret);
-		
+	}
+	
+	dfs(0, 0, 0);
+	
+	System.out.println(ret == Integer.MAX_VALUE? -1 : ret);
 	}
 	private static void dfs(int y, int x, int cnt) {
-		if(y == 9 && x > 9) {
-			ret = Math.min(cnt, ret);
+		if(cnt >= ret)return;
+		
+		if(x == 10) {
+			dfs(y+1, 0 , cnt);
+			return;
+		}
+		if(y == 10) {
+			ret = Math.min(ret, cnt);
 			return;
 		}
 		
-		if(cnt >= ret) {
-			return;
-		}
-		
-		if(x > 9) {
-			dfs(y+1, 0, cnt);
-			return;
-		}
-		
-		if(a[y][x]==1) {
-			for(int i=5;i>=1;i--) {
-				if(paper[i] > 0 && isAttach(y, x, i)) {
-					// 붙이기
-					paper[i]--;
-					attach(y, x, i, 0);
-					dfs(y, x+1, cnt+1);
-					paper[i]++;
-					attach(y, x, i, 1);
-					// 떼기
-				}
-			}
-		}else {
+		if(a[y][x] == 0) {
 			dfs(y, x+1, cnt);
+			return;
+		}
+		
+		for(int i=5;i>=1;i--) {
+			if(paper[i] == 0)continue;
+			if(!canAttach(y, x, i))continue;
+			isAttach(y, x, i,0);
+			paper[i]--;
+			dfs(y, x+i, cnt+1);
+			paper[i]++;
+			isAttach(y, x, i,1);
 		}
 	}
-	private static void attach(int y, int x, int k, int stamp) {
-		for(int i=y;i<y+k;i++) {
-			for(int j=x;j<x+k;j++) {
-				a[i][j] =stamp;
+	
+	private static void isAttach(int y, int x, int paper, int num) {
+		for(int i=y;i<y+paper;i++) {
+			for(int j=x;j<x+paper;j++) {
+				a[i][j] = num;
+			}
+		}
+	}
+	
+	private static boolean canAttach(int y, int x, int paper) {
+		if(y+paper > 10 || x+paper > 10)return false;
+		
+		for(int i=y;i<y+paper;i++) {
+			for(int j=x;j<x+paper;j++) {
+				if(a[i][j]==0)return false;
 			}
 		}
 		
-	}
-	private static boolean isAttach(int y, int x, int k) {
-		for(int i=y;i<y+k;i++) {
-			for(int j=x;j<x+k;j++) {
-				if(i >= 10 || j >= 10 || a[i][j] == 0)return false;
-			}
-		}
 		return true;
 	}
-
-	
 }
