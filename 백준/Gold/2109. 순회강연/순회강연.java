b1be0576f9e2ec09df52e2f769368e56f7c7c765
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-07 09:13:09 +0900

    [Gold III] Title: 순회강연, Time: 240 ms, Memory: 20452 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -1,50 +1,37 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, p, d;
+	static int N, a[][];
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		N = Integer.parseInt(br.readLine());
-		int a[][] = new int[N][2];
+		a = new int[N][2];
 		for(int i=0;i<N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			a[i][0] = Integer.parseInt(st.nextToken());
 			a[i][1] = Integer.parseInt(st.nextToken());
+			a[i][0] = Integer.parseInt(st.nextToken());
 		}
-		
-		Arrays.sort(a, new Comparator<int[]>() {
+		Arrays.sort(a, (o1, o2)-> Integer.compare(o1[0], o2[0]));
 
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[1]-o2[1];
-			}
-		});
-		
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		
-		for(int i=0;i<N;i++) {
-			pq.offer(a[i][0]);
-			
-			while(pq.size() > a[i][1]) {
-				pq.poll();
-			}
-		}
-		
-		int ret = 0;
-		while(!pq.isEmpty())ret += pq.poll();
-		
-		System.out.println(ret);
 
+		for(int i=0;i<N;i++) {
+			pq.offer(a[i][1]);
+			while(pq.size() > a[i][0]) {
+				pq.poll();			}
+			
+		}
+		int ret = 0;
+		while(!pq.isEmpty()) {
+			ret += pq.poll();
+		}
+		System.out.println(ret);
 	}
 
 }
