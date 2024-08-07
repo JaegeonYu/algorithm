8d8c00f64f672c0f65b2367b85450abca65de20e
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-07 10:49:45 +0900

    [Gold II] Title: 보석 도둑, Time: 1436 ms, Memory: 119484 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -7,45 +7,43 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, K;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
 		
-		int jual[][] = new int[N][2];
-		int bag[] = new int[K];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int a[][] = new int[N][2];
+		int[] b = new int[K];
 		
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
-			jual[i][0] = Integer.parseInt(st.nextToken());
-			jual[i][1] = Integer.parseInt(st.nextToken());
+			a[i][0] = Integer.parseInt(st.nextToken());
+			a[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
+
 		for(int i=0;i<K;i++) {
-			bag[i] = Integer.parseInt(br.readLine());
+			
+			b[i] = Integer.parseInt(br.readLine());
 		}
 		
-		Arrays.sort(jual, (o1, o2)->{
-			return Integer.compare(o1[0], o2[0]);
-		});
-		Arrays.sort(bag);
-		
+		Arrays.sort(a, (o1, o2) -> Integer.compare(o1[0], o2[0]));
+		Arrays.sort(b);
 		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 		
-		long ret = 0;
-		int cnt = 0;
+		int j = 0;
+		long ret  = 0;
 		
 		for(int i=0;i<K;i++) {
-			while(cnt < N && jual[cnt][0] <= bag[i]) {
-				pq.offer(jual[cnt++][1]);
-			}
-			
+			while(j<N && a[j][0] <= b[i])pq.offer(a[j++][1]);
 			if(!pq.isEmpty())ret += pq.poll();
 		}
 		
 		System.out.println(ret);
+		
 	}
 
 }
