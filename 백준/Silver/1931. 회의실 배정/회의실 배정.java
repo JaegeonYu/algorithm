1ed32179063f117949dbc96ec50dce1230f914e3
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-07 10:22:18 +0900

    [Silver I] Title: 회의실 배정, Time: 488 ms, Memory: 44260 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -5,37 +5,32 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	
+	static int N , a[][];
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		int a[][] = new int[N][2];
 		
+		a = new int[N][2];
 		for(int i=0;i<N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			a[i][0] = Integer.parseInt(st.nextToken());
 			a[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(a, (o1, o2)->{
-			if(o1[1]==o2[1]) {
-				return Integer.compare(o1[0], o2[0]);
-			}
-			return Integer.compare(o1[1], o2[1]);
+		Arrays.sort(a, (o1, o2) -> {
+			if(o1[1]==o2[1])return Integer.compare(o1[0], o2[0]);
+			else return Integer.compare(o1[1], o2[1]);
 		});
 		
-		int now = 0;
-		int ret = 0;
-
-		
-		for(int i=0;i<N;i++) {
-			if(now <= a[i][0]) {
+		int time = a[0][1];
+		int ret = 1;
+		for(int i=1;i<N;i++) {
+			if(time <= a[i][0]) {
 				ret++;
-				now = a[i][1];
+				time = a[i][1];
+				
 			}
 		}
-		
 		System.out.println(ret);
 	}
 
