7f6d8774f998ace9a7ffcb6e0ba9ff5f730edcfa
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-08 11:06:04 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,54 +6,49 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, K, plug[];
-	static boolean vis[]; 
+	static int N, K, a[];
+	static boolean vis[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		
-		plug = new int[K];
-		vis = new boolean[K+1];
-		
 		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<K;i++) {
-			plug[i] = Integer.parseInt(st.nextToken());
-		}
+		a = new int[K];
+		vis = new boolean[K+1];
+		for(int i=0;i<K;i++)a[i] = Integer.parseInt(st.nextToken());
 		
-		List<Integer> uses = new ArrayList<>();
-		int cnt = 0;
+		List<Integer> plugs = new ArrayList<>();
+		int ret = 0 ;
 		for(int i=0;i<K;i++) {
-
-			if(vis[plug[i]])continue;
-			if(uses.size() == N) {
-				// 지금 쓰는 기구 중에 나중에 안쓰는 기구 빼기
-				int last_idx =0, pos =0 ;
-				for(int use : uses) {
+			if(vis[a[i]])continue;
+			
+			if(plugs.size() == N) {
+				// 가장 안쓰는 거 빼기
+				int idx = 0;
+				int pos = 0;
+				
+				for(int plug: plugs) {
 					int here = Integer.MAX_VALUE;
-					for(int j=i+1;j<K;j++) {
-						if(plug[j] == use) {
+					for(int j =i+1; j<K;j++) {
+						if(plug == a[j]) {
 							here = j;
 							break;
 						}
 					}
-					if(last_idx < here) {
-						last_idx = here;
-						pos = use;
+					if(idx < here) {
+						idx = here;
+						pos = plug;
 					}
 				}
 				vis[pos] = false;
-				uses.remove(Integer.valueOf(pos));
-				cnt++;
-				
-				// 모두 안 쓰면 맨앞에 있는 것 빼기
+				plugs.remove(Integer.valueOf(pos));
+				ret++;
 			}
-				// 일단 추가
-			vis[plug[i]] = true;
-			uses.add(plug[i]);
+			vis[a[i]] = true;
+			plugs.add(a[i]);
 		}
-		System.out.println(cnt);
+		System.out.println(ret);
 	}
 
 }
