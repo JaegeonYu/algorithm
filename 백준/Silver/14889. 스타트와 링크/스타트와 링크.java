bdc8f4413d12682a2fa7848d3a50481c94086bd1
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-15 10:26:45 +0900

    [Silver I] Title: 스타트와 링크, Time: 332 ms, Memory: 47976 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,41 +6,51 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n, s[][] = new int[24][24], ret= Integer.MAX_VALUE;
+	static int N, a[][];
+
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		n = Integer.parseInt(st.nextToken());
-		for(int i=0;i<n;i++) {
-			st = new StringTokenizer(bf.readLine());
-			for(int j=0;j<n;j++) {
-				s[i][j]=Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		a = new int[N][N];
+		
+		int ret = Integer.MAX_VALUE;
+		
+		for(int i=0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++) {
+				a[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		for(int i=0;i<(1<<n);i++) {
-			if(Integer.bitCount(i) != n/2)continue;
-			List<Integer> start = new ArrayList<>();
-			List<Integer> link = new ArrayList<>();
-			for(int j=0;j<n;j++) {
-				if((i & (1<<j)) > 0)start.add(j);
-				else link.add(j);
+		for(int i=1;i<(1<<N);i++) {
+			if(Integer.bitCount(i) != N/2)continue;
+			List<Integer> a = new ArrayList<>();
+			List<Integer> b = new ArrayList<>();
+			
+			for(int j=0;j<N;j++) {
+				if((i & (1 << j)) > 0)a.add(j);
+				else b.add(j);
 			}
-			ret = Math.min(ret, go(start, link));
+
+			int retA = get(a);
+			int retB = get(b);
+			ret = Math.min(ret, Math.abs(retA - retB));
 		}
+		
+		
 		System.out.println(ret);
 	}
-	private static int go(List<Integer> a, List<Integer> b) {
-		int arr[ ] = new int[2];
-		for(int i=0;i<n/2;i++) {
-			for(int j=0;j<n/2;j++) {
-				if(i==j)continue;
-				arr[0]+=s[a.get(i)][a.get(j)];
-				arr[1]+=s[b.get(i)][b.get(j)];
+
+	private static int get(List<Integer> team) {
+		int ret = 0;
+		for(int i=0;i<team.size();i++) {
+			for(int j=0;j<team.size();j++) {
+				if(i == j)continue;
+				ret += a[team.get(i)][team.get(j)];
 			}
 		}
-		return Math.abs(arr[0]-arr[1]);
+		
+		return ret;
 	}
 
 }
