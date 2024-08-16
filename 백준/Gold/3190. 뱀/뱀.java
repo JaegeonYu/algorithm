f1362f2f9867d8f42eee08a872ea7561daf33bc0
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-16 11:03:01 +0900

    [Gold IV] Title: 뱀, Time: 104 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -1,63 +1,71 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n, k, l,map[][], dir[][], go=0, ret =0, now = 0;
-	static boolean vis[][];
-	static int dy[]= {0,1,0,-1};
-	static int dx[]= {1,0,-1,0}; 
+	static int N, K,L, a[][], b[][];
+	static int[] dy = {0,1,0,-1};
+	static int[] dx = {1,0,-1,0};
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(bf.readLine());
-		map = new int[n][n];
-		vis = new boolean[n][n];
-		k = Integer.parseInt(bf.readLine());
-		StringTokenizer st;
-		for(int i=0;i<k;i++) {
-			st = new StringTokenizer(bf.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		K = Integer.parseInt(br.readLine());
+		
+		a = new int[N][N];
+		
+		for(int i=0;i<K;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int y = Integer.parseInt(st.nextToken())-1;
 			int x = Integer.parseInt(st.nextToken())-1;
-			map[y][x]=1;
+			a[y][x] = 1;
 		}
-		l = Integer.parseInt(bf.readLine());
-		dir = new int[l][2];
-		for(int i=0;i<l;i++) {
-			st = new StringTokenizer(bf.readLine());
-			int tmp = Integer.parseInt(st.nextToken());
-			char direction = st.nextToken().charAt(0);
-			if(direction == 'D') {
-				dir[i]= new int[]{tmp,1};
-			}else dir[i]= new int[] {tmp,3};
-		}
-		Deque<int[]> dq = new ArrayDeque<>();
-		dq.add(new int[]{0,0});
-		while(!dq.isEmpty()) {
-			ret++;
-			int[] tmp = dq.peekFirst();
-			int ny = tmp[0] + dy[go];
-			int nx = tmp[1] + dx[go];
-			if(nx <0 || nx >=n || ny <0 || ny >=n|| vis[ny][nx])break;
-			if(map[ny][nx]==1) {
-				map[ny][nx]=0;
-			}else {
-				int out[] = dq.removeLast();
-				vis[out[0]][out[1]]=false;
-			}
-			vis[ny][nx]=true;
-			dq.addFirst(new int[] {ny, nx});
-			
-			if(now < dir.length && ret == dir[now][0]) {
-				go = (go+dir[now++][1]) % 4;
-			}
-		}
-		System.out.println(ret);
 		
+		L = Integer.parseInt(br.readLine());
+		b = new int[L][2];
+		for(int i=0;i<L;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			b[i][0] = Integer.parseInt(st.nextToken());
+			char next = st.nextToken().charAt(0);
+			if(next == 'L')b[i][1] = 3;
+			else b[i][1] = 1;
+		}
+		
+		int cnt = 0;
+		
+		int time = 0;
+		
+		ArrayDeque<int[]> dq = new ArrayDeque<>();
+		dq.offer(new int[] {0,0});
+		a[0][0] = 2;
+		int dir = 0;
+		while(!dq.isEmpty()) {
+			// 이동
+			time++;
+			int[] now = dq.peekFirst();
+			int ny = now[0]+dy[dir];
+			int nx = now[1]+dx[dir];
+			
+			if(nx < 0 || nx >= N || ny < 0 || ny >= N || a[ny][nx] == 2)break;
+			dq.offerFirst(new int[] {ny, nx});
+			if(a[ny][nx] == 1) {	
+				a[ny][nx] = 2;
+			}else {
+				a[ny][nx] = 2;
+				int[] prev = dq.pollLast();
+				a[prev[0]][prev[1]] = 0;
+			}
+	
+			
+			// 방향 전환
+			if(cnt < L && b[cnt][0] == time) {
+				dir = (dir+b[cnt][1]) % 4;
+				cnt++;
+			}
+
+		}
+		System.out.println(time);
 	}
 
 }
