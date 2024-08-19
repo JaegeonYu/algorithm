f2f1814a68281f9930f33d4b8793068408eea22f
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-19 10:19:24 +0900

    [Silver I] Title: 기타 레슨, Time: 296 ms, Memory: 24040 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -4,43 +4,49 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, arr[];
-	static long ret = Long.MAX_VALUE-1;
+	static int N, M, a[]; 
+	static long ret = Long.MAX_VALUE;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		arr = new int[N];
+		a = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++)arr[i] = Integer.parseInt(st.nextToken());
+		
+		for(int i=0;i<N;i++)a[i] = Integer.parseInt(st.nextToken());
+		
 		long le = 1;
 		long ri = Long.MAX_VALUE-1;
+		
 		while(le <= ri) {
 			long mid = (le+ri)/2;
 			if(check(mid)) {
-				ret= mid;
-				
-				ri = mid -1;
-			}else le = mid + 1;
-		}
-		System.out.println(ret);
-	}
-	private static boolean check(long mid) {
-		int cnt = 1;
-		int tmp = 0;
-		for(int i=0;i<N;i++) {
-			if(mid < arr[i])return false;
-			if(tmp + arr[i] > mid) {
-				cnt++;
-				tmp = arr[i];
+				ret = mid;
+				ri = mid - 1;
 			}else {
-				tmp += arr[i];
+				le = mid +1;
 			}
 		}
-
-		return cnt <= M;
+		System.out.println(ret);
+		
+	}
+	private static boolean check(long mid) {
+		int ret = 1;
+		int now = 0;
+		for(int i=0;i<N;i++) {
+			if(mid < a[i])return false;
+			
+			if(now+a[i] > mid) {
+				ret++;
+				now = 0;
+			}
+			
+			now += a[i];
+		}
+		
+		return ret <= M;
 	}
 
 }
