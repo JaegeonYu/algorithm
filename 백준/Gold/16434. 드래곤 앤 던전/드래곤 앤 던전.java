628ad203164ee65ed222962af8ff7116bfa0a0ae
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-20 10:17:43 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 520 ms, Memory: 53456 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
@@ -4,50 +4,68 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static long n, atk, dng[][];
-	static long ret;
+	static int N, atk;
+	static Dungeon[] dungeons;
+	static long ret = Integer.MAX_VALUE;
+	static class Dungeon{
+		int t;
+		int a;
+		int h;
+		public Dungeon(int t, int a, int h) {
+			super();
+			this.t = t;
+			this.a = a;
+			this.h = h;
+		}
+		
+	}
 	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		n = Integer.parseInt(st.nextToken());
-		dng = new long[(int)n][3];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
 		atk = Integer.parseInt(st.nextToken());
-		for(int i=0;i<n;i++) {
-			st = new StringTokenizer(bf.readLine());
-			int check = Integer.parseInt(st.nextToken());
+		
+		dungeons = new Dungeon[N];
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			int t = Integer.parseInt(st.nextToken());
 			int a = Integer.parseInt(st.nextToken());
 			int h = Integer.parseInt(st.nextToken());
-			dng[i] = new long[]{check, a, h};
+			dungeons[i] = new Dungeon(t, a, h);
 		}
-		long le =0, ri = Long.MAX_VALUE;
+		
+		long le = 1;
+		long ri = Long.MAX_VALUE-1;
+		
 		while(le <= ri) {
 			long mid = (le+ri)/2;
+
 			if(check(mid)) {
-				ri = mid -1;
 				ret = mid;
+				ri = mid -1;
 			}else le = mid +1;
 		}
 		System.out.println(ret);
 	}
 	private static boolean check(long mid) {
-		long tmpAtk = atk;
-		long initHp = mid;
-		
-		for(int i=0;i<n;i++) {
-			if(dng[i][0]==1) {
-				long monHp = dng[i][2];
-				long div = monHp / tmpAtk;
-				if(monHp % tmpAtk > 0)div++;
-				mid -= (div-1) * dng[i][1];
+		long now = mid;
+		long nowAtk = atk;
+		for(int i=0;i<N;i++) {
+			if(dungeons[i].t == 1) {
+				int h = dungeons[i].h;
+				
+				long div = h/nowAtk;
+				if(h % nowAtk > 0)div++;
+				now -= (div-1) * dungeons[i].a;
 				
 			}else {
-				tmpAtk += dng[i][1];
-				mid = Math.min(initHp, dng[i][2]+mid);
+				nowAtk += dungeons[i].a;
+				now = Math.min(mid, now+dungeons[i].h);
 			}
-			
-			if(mid<=0)return false;
+			if(now <= 0)return false;
 		}
-		return true;
+
+		return now > 0;
 	}
 
 }
