b1395ef9d86df3dd9571e6b8696da4df7fdf1f72
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-21 10:02:22 +0900

    [Gold I] Title: 놀이 공원, Time: 140 ms, Memory: 14976 KB -BaekjoonHub

--- 백준/Gold/1561. 놀이 공원/놀이 공원.java
+++ 백준/Gold/1561. 놀이 공원/놀이 공원.java
@@ -5,8 +5,8 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M, a[];
+	static long ret;
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
@@ -14,48 +14,46 @@ public class Main {
 		a = new int[M];
 		
 		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<M;i++) {
-			a[i] = Integer.parseInt(st.nextToken());
-		}
+		for(int i=0;i<M;i++)a[i] = Integer.parseInt(st.nextToken());
 		
-		long le = 0;
-		long ri = 2_000_000_000L * 30 + 1;
-		if(N<=M) {
+		if(N <= M) {
 			System.out.println(N);
 			return;
 		}
 		
-		long ret = 0;
+		long le = 1;
+		long ri = 2_000_000_000L * 30 + 1;
 		while(le <= ri) {
 			long mid = (le+ri)/2;
-			if(checkMid(mid)) {
-				ret = mid;
-				ri = mid - 1;
-				
+			if(check(mid)) {
+				ret = mid ;
+				ri = mid -1;
 			}else {
-				le = mid +1;
+				le = mid + 1;
 			}
 		}
-		long temp = M;
+		
+		long tmp = M;
 		for(int i=0;i<M;i++) {
-			temp+=(ret-1)/a[i];
+			tmp += ((ret-1) / a[i]);
 		}
-
+		
 		for(int i=0;i<M;i++) {
-			if(ret % a[i] == 0) temp++;
-			if(temp == N) {
+			if(ret % a[i] == 0)tmp++;
+			if(tmp == N) {
 				System.out.println(i+1);
 				break;
 			}
 		}
 		
+
 	}
-	private static boolean checkMid(long mid) {
-		long cnt = M;
-		for(int i=0;i<M;i++) {
-			cnt += mid / a[i];
-		}
-		return cnt >= N;
+	private static boolean check(long mid) {
+		long sum  = M;
+		for(int i=0;i<M;i++)sum += mid / a[i];
+		
+		
+		return sum >= N;
 	}
 
 }
