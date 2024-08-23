d3f0ed17ae9ee67a1300acbade01297f1218d978
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-23 10:24:56 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -16,35 +16,34 @@ public class Main {
 				a[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		
 		// 0 -> 가로, 1-> 세로, 2-> 대각
 		dp[0][1][0] = 1;
-		for(int i=0;i<N;i++) {
+		
+		for(int i=2;i<N;i++) {
+			if(a[0][i] == 1)continue;
+			dp[0][i][0] += dp[0][i-1][0];
+		}
+		
+		for(int i=1;i<N;i++) {
 			for(int j=2;j<N;j++) {
 				if(a[i][j] == 1)continue;
-				
-				if(i == 0) {
-					dp[i][j][0]=dp[i][j-1][0];
-				}else {
-					dp[i][j][0] = dp[i][j-1][2]+dp[i][j-1][0];
-					dp[i][j][1] = dp[i-1][j][2]+dp[i-1][j][1];
-					if(a[i-1][j] != 1 && a[i][j-1] != 1 && a[i-1][j-1] != 1)dp[i][j][2] = dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2];
-				}
+				dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2];
+				dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
+				if(a[i-1][j-1] != 1 && a[i-1][j] != 1 && a[i][j-1] != 1)dp[i][j][2] = dp[i-1][j-1][1] + dp[i-1][j-1][0] + dp[i-1][j-1][2];
 			}
 		}
 //		for(int i=0;i<N;i++) {
 //			for(int j=0;j<N;j++) {
 //				int sum = 0;
-//				for(int k=0;k<3;k++) {
+//				for(int k= 0;k<3;k++) {
 //					sum += dp[i][j][k];
 //				}
 //				System.out.print(sum+" ");
 //			}
 //			System.out.println();
 //		}
-		
-		System.out.println(dp[N-1][N-1][0]+dp[N-1][N-1][1]+dp[N-1][N-1][2] );
-		
-
+		System.out.println(dp[N-1][N-1][0]+dp[N-1][N-1][1]+dp[N-1][N-1][2]);
 	}
 
 }
