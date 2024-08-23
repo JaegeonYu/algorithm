62bb4d5c7aa3465c9400c995ab3d50ed78c0e940
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-23 09:57:39 +0900

    [Gold I] Title: 외판원 순회, Time: 184 ms, Memory: 20168 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -5,44 +5,45 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, w[][], dp[][], ret = Integer.MAX_VALUE;
-	static boolean vis[];
-	 private static final int INF = 987654321;
-	public static void main(String[] args) throws IOException {
+	static int MAX_N = 16;
+	static int INF = 987654321;
+	static int N, a[][], dp[][] = new int[MAX_N][1 << MAX_N];
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		
-		vis = new boolean[N];
-		w = new int[N][N];
-		dp = new int[N][1 << N];
+		a = new int[N][N];
 		
 		for(int i=0;i<N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j=0;j<N;j++) {
-				w[i][j] = Integer.parseInt(st.nextToken());
-			
+				a[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
+		
 		for(int i=0;i<N;i++)Arrays.fill(dp[i], -1);
-	
+		
 		System.out.println(tsp(0, 1));
+		
+		
 	}
 	private static int tsp(int here, int visited) {
-		if(visited == (1 << N) -1) {
-			return w[here][0] != 0 ? w[here][0] : INF;
+		if(visited == (1 << N) - 1) {
+			return a[here][0] != 0? a[here][0] : INF;
 		}
 		
-		if(dp[here][visited] != -1)return dp[here][visited];
+		if(dp[here][visited] != -1) {
+			return dp[here][visited];
+		}
 		
-		dp[here][visited] =  INF;
+		dp[here][visited] = INF;
 		for(int i=0;i<N;i++) {
-			if((visited & (1<< i)) != 0)continue;
-			if(w[here][i] == 0)continue;
-			dp[here][visited] = Math.min(dp[here][visited], tsp(i, visited | (1<<i)) + w[here][i]);
+			if((visited & ( 1<< i)) != 0)continue;
+			if(a[here][i] == 0)continue;
+			
+			dp[here][visited] = Math.min(dp[here][visited], tsp(i, visited | (1 << i))+a[here][i]);
 		}
 		return dp[here][visited];
 	}
-
 
 }
