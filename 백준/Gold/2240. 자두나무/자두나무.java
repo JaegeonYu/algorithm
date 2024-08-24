507c59cd29dbf8aef546cb67790950ad671faf25
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-24 09:56:08 +0900

    [Gold V] Title: 자두나무, Time: 104 ms, Memory: 14852 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -10,27 +10,26 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		T = Integer.parseInt(st.nextToken());
 		W = Integer.parseInt(st.nextToken());
+		
+		dp = new int[T][2][W+1];
 		a = new int[T];
-		
 		for(int i=0;i<T;i++) {
-			a[i] = Integer.parseInt(br.readLine());
+			a[i] = Integer.parseInt(br.readLine())-1;
 		}
-		dp = new int[1004][2][34];
-		System.out.println(Math.max(max(0, 1, W-1), max(0, 0, W)));
 		
+		System.out.println(Math.max(go(0, 0, W), go(0, 1, W-1)));
 
 	}
-	private static int max(int idx, int tree, int cnt) {
+	private static int go(int idx, int here, int cnt) {
 		if(cnt < 0)return Integer.MIN_VALUE;
 		if(idx == T)return 0;
-		
-		int ret = dp[idx][tree][cnt];
+		int ret = dp[idx][here][cnt];
 		if(ret > 0)return ret;
 		
-		dp[idx][tree][cnt] = Math.max(max(idx+1, tree, cnt), max(idx+1, tree^1, cnt-1));
-		if(tree == a[idx] -1)dp[idx][tree][cnt]++;
-				
-		return dp[idx][tree][cnt];
+		dp[idx][here][cnt] = Math.max(go(idx+1, here, cnt), go(idx+1, here ^ 1, cnt -1));
+		if(here == a[idx])dp[idx][here][cnt]++;
+		
+		return dp[idx][here][cnt];
 	}
 
 }
