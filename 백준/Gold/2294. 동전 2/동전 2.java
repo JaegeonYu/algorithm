015b1e73d8cee23ad5e5f8debf72ec2f9f667bd1
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-26 09:59:19 +0900

    [Gold V] Title: 동전 2, Time: 120 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -5,26 +5,26 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N,K ,dp[];
+	static int N, K, dp[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
+
 		dp = new int[100004];
-		
-		Arrays.fill(dp, Integer.MAX_VALUE-5);
+		Arrays.fill(dp, 100004);
 		for(int i=0;i<N;i++) {
-			int now = Integer.parseInt(br.readLine());
-			dp[now] = Math.min(dp[now], 1);
+			int now  = Integer.parseInt(br.readLine());
+			dp[now] = 1;
 			for(int j=now;j<=K;j++) {
 				dp[j] = Math.min(dp[j], dp[j-now]+1);
 			}
 		}
-		
-		if(dp[K] == Integer.MAX_VALUE-5)System.out.println(-1);
+//		System.out.println(Arrays.toString(dp));
+		if(dp[K] == 100004)System.out.println(-1);
 		else System.out.println(dp[K]);
-				
+		
 	}
 
 }
