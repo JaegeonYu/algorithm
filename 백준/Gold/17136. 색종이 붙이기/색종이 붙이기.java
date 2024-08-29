305174f97d8aefee004585df0f2ddca76cfe6068
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-29 10:57:21 +0900

    [Gold II] Title: 색종이 붙이기, Time: 204 ms, Memory: 24792 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -4,68 +4,76 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int a[][], ret = Integer.MAX_VALUE;
-	static int[] paper = {0,5,5,5,5,5};
+	static int[][] map = new int[10][10];
+	static int paper[] = {0,5,5,5,5,5};
+	static int ret = Integer.MAX_VALUE;
+	static boolean vis[][] = new boolean[10][10];
+	
 	public static void main(String[] args) throws IOException {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-	a = new int[10][10];
-	for(int i=0;i<10;i++) {
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int j=0;j<10;j++) {
-			a[i][j] = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for(int i=0;i<10;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0;j<10;j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
+		
+		dfs(0, 0, 0);
+		System.out.println(ret != Integer.MAX_VALUE ? ret : -1);
+		
 	}
-	
-	dfs(0, 0, 0);
-	
-	System.out.println(ret == Integer.MAX_VALUE? -1 : ret);
-	}
+
 	private static void dfs(int y, int x, int cnt) {
 		if(cnt >= ret)return;
+
 		
 		if(x == 10) {
-			dfs(y+1, 0 , cnt);
+			dfs(y+1, 0, cnt);
 			return;
 		}
+		
 		if(y == 10) {
 			ret = Math.min(ret, cnt);
 			return;
 		}
-		
-		if(a[y][x] == 0) {
+		if(map[y][x] == 0) {
 			dfs(y, x+1, cnt);
 			return;
 		}
+
 		
 		for(int i=5;i>=1;i--) {
 			if(paper[i] == 0)continue;
-			if(!canAttach(y, x, i))continue;
-			isAttach(y, x, i,0);
+			if(!check(y, x, i))continue;
+			paper(y, x, i, 0);
 			paper[i]--;
-			dfs(y, x+i, cnt+1);
+			// dfs
+			dfs(y, x+1, cnt+1);
 			paper[i]++;
-			isAttach(y, x, i,1);
+			// vis uncheck
+			paper(y, x, i, 1);
 		}
+		
 	}
-	
-	private static void isAttach(int y, int x, int paper, int num) {
-		for(int i=y;i<y+paper;i++) {
-			for(int j=x;j<x+paper;j++) {
-				a[i][j] = num;
+
+	private static void paper(int y, int x, int cnt, int value) {
+		for(int i=y;i<y+cnt;i++) {
+			for(int j=x;j<x+cnt;j++) {
+				map[i][j] = value;
 			}
 		}
 	}
-	
-	private static boolean canAttach(int y, int x, int paper) {
-		if(y+paper > 10 || x+paper > 10)return false;
-		
-		for(int i=y;i<y+paper;i++) {
-			for(int j=x;j<x+paper;j++) {
-				if(a[i][j]==0)return false;
+
+	private static boolean check(int y, int x, int cnt) {
+		if(y+cnt > 10 || x+cnt > 10)return false;
+		for(int i=y;i<y+cnt;i++) {
+			for(int j=x;j<x+cnt;j++) {
+				if(map[i][j] == 0)return false;
 			}
 		}
 		
 		return true;
+		
 	}
+
 }
