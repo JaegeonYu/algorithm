50a4a599e4edbe1acac67a20100d2ab33cd7f36b
Jack <76714219+JaegeonYu@users.noreply.github.com>
2024-08-30 10:42:08 +0900

    [Gold II] Title: 새로운 게임 2, Time: 144 ms, Memory: 16428 KB -BaekjoonHub

--- 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.java
+++ 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.java
@@ -7,11 +7,31 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, K, a[][];
-	static List<Integer> map[][];
-	static int[] dy = {0,0,-1,1};
-	static int[] dx = {1,-1,0,0};
+	static int N, K, map[][];
+	static List<Integer>[][] adj;
 	static List<Horse> horses = new ArrayList<>();
+	static int dx[] = {1,-1,0,0};
+	static int dy[] = {0,0,-1,1};
+	static class Horse{
+		int y;
+		int x;
+		int dir;
+
+		public Horse(int y, int x, int dir) {
+			super();
+			this.y= y;
+			this.x=x;
+			this.dir = dir;
+		}
+		
+		void reverse(){
+			if(dir == 0)dir = 1;
+			else if(dir == 1)dir = 0;
+			else if(dir ==2)dir =3;
+			else if(dir ==3)dir = 2;
+		}
+		
+	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -19,18 +39,14 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		
-		a = new int[N][N];
-		map = new ArrayList[N][N];
+		map = new int[N][N];
+		adj = new ArrayList[N][N];
 		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				map[i][j] = new ArrayList<>();
-			}
-		}
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0;j<N;j++) {
-				a[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = Integer.parseInt(st.nextToken());
+				adj[i][j] = new ArrayList<>();
 			}
 		}
 		
@@ -39,81 +55,64 @@ public class Main {
 			int y = Integer.parseInt(st.nextToken())-1;
 			int x = Integer.parseInt(st.nextToken())-1;
 			int dir = Integer.parseInt(st.nextToken())-1;
-			map[y][x].add(i);
 			horses.add(new Horse(y, x, dir));
+			adj[y][x].add(i);
 		}
 		
-		int cnt = 0;
-		boolean finish = false;
+		int ret = 0;
+		boolean finish =false;
 		while(true) {
-			
 			for(int i=0;i<horses.size();i++) {
 				int y = horses.get(i).y;
 				int x = horses.get(i).x;
 				int dir = horses.get(i).dir;
 				move(y, x, dir, i);
 				if(isOver()) {
-					finish=true;
+					finish = true;
 					break;
 				}
 			}
-			cnt++;
-			if(cnt == 1000)break;
+			ret++;
+			if(ret == 1000)break;
 			if(finish)break;
 		}
 		
-		if(finish)System.out.println(cnt);
+		if(finish)System.out.println(ret);
 		else System.out.println(-1);
 	}
-	
-	private static void move(int y, int x, int dir, int i) {
-		int ny = y +dy[dir];
-		int nx = x+dx[dir];
-		if(nx <0 || nx >= N || ny < 0 || ny >= N || a[ny][nx] ==2) {
-			horses.get(i).dir = reverse(dir);
-			ny = y+dy[horses.get(i).dir];
-			nx = x+dx[horses.get(i).dir];
-			if(nx <0 || nx >= N || ny < 0 || ny >= N || a[ny][nx] ==2)return;
-		}
-		
-		int pos = map[y][x].indexOf(Integer.valueOf(i));
-		if(a[ny][nx] == 1)Collections.reverse(map[y][x].subList(pos, map[y][x].size()));
-		for(int j = pos; j<map[y][x].size();j++) {
-			horses.get(map[y][x].get(j)).y = ny;
-			horses.get(map[y][x].get(j)).x = nx;
-			map[ny][nx].add(map[y][x].get(j));
-		}
-		while(map[y][x].size() > pos) {
-			map[y][x].remove(map[y][x].size()-1);
-		}
-		
-	}
-
-	private static int reverse(int dir) {
-		return dir^1;
-	}
-
 	private static boolean isOver() {
 		for(int i=0;i<N;i++) {
 			for(int j=0;j<N;j++) {
-				if(map[i][j].size()>=4)return true;
+				if(adj[i][j].size()>=4)return true;
 			}
 		}
 		return false;
 	}
-
-	static class Horse{
-		int y;
-		int x;
-		int dir;
-		Horse(int y, int x ,int dir){
-			this.y=y;
-			this.x=x;
-			this.dir =dir;
+	private static void move(int y, int x, int dir, int i) {
+		int ny = y+dy[dir];
+		int nx = x+dx[dir];
+		if(nx < 0 || nx >= N || ny < 0 || ny >= N || map[ny][nx] == 2) {
+			horses.get(i).reverse();
+			ny = y+dy[horses.get(i).dir];
+			nx = x+dx[horses.get(i).dir];
+			if(nx < 0 || nx >= N || ny < 0 || ny >= N || map[ny][nx] == 2)return;
 		}
 		
-		boolean check(int y, int x) {
-			return y == this.y && x == this.x;
+		int pos = adj[y][x].indexOf(Integer.valueOf(i));
+		if(map[ny][nx] == 1)Collections.reverse(adj[y][x].subList(pos, adj[y][x].size()));
+		for(int j=pos;j<adj[y][x].size();j++) {
+			
+			horses.get(adj[y][x].get(j)).y = ny;
+			horses.get(adj[y][x].get(j)).x = nx;
+			adj[ny][nx].add(adj[y][x].get(j));
 		}
+		while(adj[y][x].size() > pos) {
+			adj[y][x].remove(adj[y][x].size()-1);
+		}
+		
 	}
+	private static int reverse(int dir) {
+		return dir^1;
+	}
+
 }
